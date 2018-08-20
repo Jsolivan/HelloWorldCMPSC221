3717d0b4ef165607226be134f62b84f06d90e808
Jsolivan <42556552+Jsolivan@users.noreply.github.com>
2018-08-20 17:53:32 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,17 @@
-// John Doe
-
-public class Main
+public class Main 
 {
-
-   public static void main(String[] args)
-   {
-      System.out.println("hello world");
-   }
-
+  
+  public static void main(String[] args) 
+  {
+    System.out.println("Poker Quotes:");
+    System.out.println("A man with money");
+    System.out.println("is no match against ");
+    System.out.println("a man on a mission");
+    System.out.println("-Doyle Brunson");
+    System.out.println("");
+    System.out.println("Poker is a skill game");
+    System.out.println("pretending to be a chance game");
+    System.out.println("-James Altucher");
+  }
+  
 }
